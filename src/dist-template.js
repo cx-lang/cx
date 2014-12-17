20859a0e8e1c73550b3963f1904e00b2ffa2918e
futagoza <futagoza.ryuu@gmail.com>
2014-12-17 22:17:31 +0200

    fixed github url

--- src/dist-template.js
+++ src/dist-template.js
@@ -1,6 +1,6 @@
 /*
   Copyright (c) 2014 Futago-za Ryuu <futagoza.ryuu@gmail.com>
-  https://github.com/futagoza/cx, v__VERSION__
+  https://github.com/erispa/cx, v__VERSION__
   
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
