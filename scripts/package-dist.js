34f1ef6831ed5a5c363b6f16891209f287d9f36b
futagoza <futagoza.ryuu@gmail.com>
2014-12-17 04:48:22 +0200

    added browser packager script

---
+++ scripts/package-dist.js
@@ -0,0 +1,32 @@
+require('./globals');
+var version = require('../package.json').version;
+
+var modules = [];
+
+walk(LIB_DIR, function(filename, stat){
+  modules.push(
+    "'" + filename.replace(LIB_DIR, '') + "': function ( require, exports, module ) {\n" + 
+      indent(readFile(filename)).replace(/require\s*?\(\s*?("|')(.*)("|')\s*?\)/g, function(m, openOp, request, closeOp, index){
+        var location = 'require statement in "' + filename + '" at ' + index;
+        if ( openOp !== closeOp ) {
+          throw new SyntaxError(location + ' has mismatched opening and closing characters.');
+        }
+        var first = request.charAt(0);
+        if ( first === '/' || first === '\\' ) {
+          throw new SyntaxError(location + ' is pointing to an absolute path.');
+        }
+        if ( first === '.' ) {
+          request = resolve(stat.dirname, request).replace(LIB_DIR, 'cx').replace(/\\/g, "/");
+        }
+        return "require(" + openOp + request + closeOp + ")";
+      })
+    + "\n}"
+  );
+});
+
+var output = readFile(join(SRC_DIR, 'dist-template.js'))
+  .replace('__MODULES__', modules.join(",\n\n"))
+  .replace('__VERSION__', version);
+
+writeFIle(join(DIST_DIR, 'cx-' + version + '.js'), output);
+writeFIle(join(DIST_DIR, 'cx-' + version + '.min.js'), minify(output));
